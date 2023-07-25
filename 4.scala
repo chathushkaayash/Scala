30c06336a69b50216cc88a8155cace66c7f53816
M.C.A. Maddumage <102851155+chathushkaayash@users.noreply.github.com>
2023-07-25 06:49:18 +0530

    Delete 4.scala

--- 4.scala
+++
@@ -1,9 +0,0 @@
-object q4 extends App{
-    def evenOrodd(s:Int):String = s match{
-        case 1 => "Odd"
-        case 0 => "Even"
-        case _ => evenOrodd(s-2)
-    }
-
-    println(evenOrodd(7))
-}
