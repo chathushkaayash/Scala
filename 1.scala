ca022fe27a4574c52e18ef7a122cddcb74380787
M.C.A. Maddumage <102851155+chathushkaayash@users.noreply.github.com>
2023-07-25 06:47:32 +0530

    Update 1.scala

--- 1.scala
+++ 1.scala
@@ -1,15 +1,48 @@
-object q1 extends App{
+import scala.io.StdIn.readLine
+import scala.io.StdIn
 
-    def GCD(a:Int, b:Int):Int = b match{
-        case 0 => a
-        case x if x>a => GCD(x,a)
-        case _ => GCD(b,a%b)
-    }
-    def checkPrime(p:Int, n:Int = 2):Boolean = n match{
-        case x if(x == p) => true
-        case x if(GCD(p,x) > 1) => false
-        case x => checkPrime(p,x+1)
-    }
+object StringToList extends App {
+  println("Enter the name: ")
+  val name = readLine()
+  println("Enter the shift amount: ")
+  val shift = StdIn.readInt()
 
-    println(checkPrime(3))
+  // Convert the string to a list of characters using map
+  val inputList: List[Char] = name.toList
+
+  // Print the resulting list
+  println(inputList)
+
+  def encryptChar(char: Char, shift: Int): Char = {
+    if (char.isLetter) {
+      val base = if (char.isUpper) 'A' else 'a'
+      ((char - base + shift + 26) % 26 + base).toChar
+    } else {
+      char
+    }
+  }
+
+  def intoEncrypted(charList: List[Char], shift: Int): String = {
+    val encryptedChars: List[Char] = charList.map(char => encryptChar(char, shift))
+    encryptedChars.mkString
+  }
+
+  // Changed the function name to "intoEncrypted"
+  println(intoEncrypted(inputList, shift))
+
+  println("Enter the name: ")
+  val name2 = readLine()
+  println("Enter the shift amount: ")
+  val shift2 = StdIn.readInt()
+
+  val inputList2: List[Char] = name2.toList
+
+  def intoDecrypted(charList: List[Char], shift: Int): String = {
+    // Changed the function name to "intoDecrypted"
+    val decryptedChars: List[Char] = charList.map(char => encryptChar(char, -shift))
+    decryptedChars.mkString
+  }
+
+  // Changed the function name to "intoDecrypted"
+  println(intoDecrypted(inputList2, shift2))
 }
