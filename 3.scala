f81fd3e20df2f9323c2a3445ad83c3785b9eb951
M.C.A. Maddumage <102851155+chathushkaayash@users.noreply.github.com>
2023-08-15 07:33:32 +0530

    Update 3.scala

--- 3.scala
+++ 3.scala
@@ -1,21 +1,12 @@
 object Q3 extends App{
-  val numbers = filterPrime(Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-  println(numbers.mkString(", "));
 
+    val toUpper: String => String = str => str.toUpperCase
+    val toLower: String => String = str => str.toLowerCase
+    val formatNames: (String, String => String) => String = (name, formatFunc) => formatFunc(name)
 
-  def filterPrime(arr: Array[Int]): Array[Int] = {
-    arr.filter(arr => prime(arr));
-  }
-
-  def gcd(a: Int, b: Int): Int = a match {
-    case a if (a == 0) => b
-    case _ => gcd(b % a, a)
-  }
-
-  def prime(p: Int, n: Int = 2): Boolean = n match {
-    case x if (p == 1) => false
-    case x if (p == x) => true
-    case x if gcd(p, x) > 1 => false
-    case x => prime(p, x + 1)
-  }
+    
+    println(formatNames("Benny",toUpper))
+    println(formatNames("Ni",toUpper)+"roshan")
+    println(formatNames("Saman",toLower))
+    println("Kumar"+formatNames("a",toUpper))
 }
