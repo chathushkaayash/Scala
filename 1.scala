52d09bbdc698084b380a01c93d19f9f09f2e852a
M.C.A. Maddumage <102851155+chathushkaayash@users.noreply.github.com>
2023-08-22 06:10:16 +0530

    Delete 1.scala

--- 1.scala
+++
@@ -1,9 +0,0 @@
-object Q1 extends App{
-    val calculateInterest: Double => Double = depositAmount =>
-        if (depositAmount <= 20000) 0.02 * depositAmount
-        else if (depositAmount <= 200000) 0.04 * depositAmount
-        else if (depositAmount <= 2000000) 0.035 * depositAmount
-        else 0.065 * depositAmount
-
-    println(calculateInterest(25000))
-}
