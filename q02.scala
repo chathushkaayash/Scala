eb02c24d051ba3b279083ae8dd15740cba2bc296
M.C.A. Maddumage <102851155+chathushkaayash@users.noreply.github.com>
2023-08-29 08:28:24 +0530

    Update q02.scala

--- q02.scala
+++ q02.scala
@@ -1,31 +1,10 @@
-class RationalNumber2(n: Int, d: Int) {
-  require(d != 0, "Denominator must be nonzero")
+object q2 extends App{
+    def countLetterOccurrences(words: List[String]): Int = {
+    val wordLengths = words.map(_.length) 
+    val totalLetterCount = wordLengths.reduce(_ + _) 
+    totalLetterCount
+    }
 
-  private val gcdValue = gcd(n.abs, d.abs)
-  val numerator: Int = n / gcdValue
-  val denominator: Int = d / gcdValue
-
-  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
-
-  def neg: RationalNumber2 = new RationalNumber2(-numerator, denominator)
-
-  def sub(other: RationalNumber2): RationalNumber2 =
-    new RationalNumber2(numerator * other.denominator - other.numerator * denominator, denominator * other.denominator)
-
-  override def toString: String = s"$numerator / $denominator"
-}
-
-object RationalNumberQ02 {
-  def main(args: Array[String]): Unit = {
-    val x = new RationalNumber2(3, 4)
-    val y = new RationalNumber2(5, 8)
-    val z = new RationalNumber2(2, 7)
-
-    val result = x.sub(y).sub(z)
-
-    println(s"x: $x")
-    println(s"y: $y")
-    println(s"z: $z")
-    println(s"Result of x - y - z: $result")
-  }
+    val words = List("apple", "banana", "cherry", "date")
+    print(countLetterOccurrences(words))
 }
