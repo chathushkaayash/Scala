84877c327ba26dd751afcdfcff37fdfdd96b89b2
M.C.A. Maddumage <102851155+chathushkaayash@users.noreply.github.com>
2023-08-29 08:21:34 +0530

    Update q01.scala

--- q01.scala
+++ q01.scala
@@ -1,23 +1,11 @@
-class RationalNumber1(n: Int, d: Int) {
-  require(d != 0, "Denominator must be nonzero")
-
-  private val gcdValue = gcd(this.n, this.d)
-  val numerator: Int = n / gcdValue
-  val denominator: Int = d / gcdValue
-
-  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
-
-  def neg: RationalNumber1 = new RationalNumber1(-numerator, denominator)
-
-  override def toString: String = s"$numerator / $denominator"
-}
-
-object RationalNumberQ01 {
-  def main(args: Array[String]): Unit = {
-    val x = new RationalNumber1(2, 3)
-    val y = x.neg
-
-    println(s"x: $x")
-    println(s"Negative of x: $y")
+object q1 extends App{
+    def calculateAverage(temperatures: List[Double]): Double = {
+    val fahrenheitTemperatures = temperatures.map(celsius => (celsius * 9.0/5.0) + 32)
+    val totalFahrenheit = fahrenheitTemperatures.reduce((temp1, temp2) => temp1 + temp2)
+    val averageFahrenheit = totalFahrenheit / temperatures.length.toDouble
+    averageFahrenheit
   }
+    val celsiusTemperatures = List(0.0,10.0,20.0,30.0)
+    
+    println(calculateAverage(celsiusTemperatures))
 }
